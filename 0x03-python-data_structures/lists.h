b72d4e390ab52675d1a2d39710d0007e15becc93
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-08-14 09:08:17 -0700

    task0

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+def print_list_integer(my_list=[]);
+def element_at(my_list, idx);
+def replace_in_list(my_list, idx, element);
+
+#endif
