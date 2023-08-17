1423d092707eb7b3105cb611663267df156c6639
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-08-16 19:49:04 -0700

    task8

--- 0x04-python-more_data_structures/8-main.py
+++ 0x04-python-more_data_structures/8-main.py
@@ -8,6 +8,7 @@ new_dict = simple_delete(a_dictionary, 'track')
 print_sorted_dictionary(a_dictionary)
 print("--")
 print_sorted_dictionary(new_dict)
+
 print("--")
 print("--")
 new_dict = simple_delete(a_dictionary, 'c_is_fun')
