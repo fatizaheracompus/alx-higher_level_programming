5ced737a390d5d88ef2b03b73b9762909b717438
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2023-08-18 19:40:36 -0700

    task12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    n = 0
+    valeur = 0
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for rom in reversed(roman_string):
+        n = digits[rom]
+        valeur += n if valeur < n * 5 else -n
+        return valeur
