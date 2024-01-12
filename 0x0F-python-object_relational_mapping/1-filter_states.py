1bf57fe1409f1b824cd9ced65f3f7d59f6138de1
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2024-01-12 10:05:33 -0800

    rr

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" Script list all states from the database hbtn_0e_0_usa """
+"""  lists all states from the database hbtn_0e_0_usa """
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states ORDER BY states.id""")
+    cursor.execute("""SELECT * FROM state WHERE name
+                LIKE BINARY 'N%' ORDER BY id""")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
