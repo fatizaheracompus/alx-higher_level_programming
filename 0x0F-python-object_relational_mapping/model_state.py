89b61f7620e3fe038e576fabe673ced92d39668b
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2024-01-14 14:49:40 -0800

    r

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""  python file that contains the class definition of a State
+"""Start link class to table in database
 """
-
 import sys
 from model_state import Base, State
 
