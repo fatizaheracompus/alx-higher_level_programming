275b3e07f6e316ccc9d789c62cf7b8c56cb2de00
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2024-01-26 16:38:11 -0800

    t

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL and displays the value
+Python script that takes in a URL, sends a request to the URL and displays
+the value.
 """
 import urllib.request
 import sys
@@ -9,5 +10,5 @@ import sys
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         html = response.info()
-        value = html.get('X-Request-Id')
-        print(value)
+        val = html.get('X-Request-Id')
+        print(val)
