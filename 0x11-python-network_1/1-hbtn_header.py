5d2e711284e928a32bc9453d533a829289b53a38
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2024-01-26 16:26:52 -0800

    t

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,5 +9,5 @@ import sys
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         html = response.info()
-        val = html.get('X-Request-Id')
-        print(val)
+        value = html.get('X-Request-Id')
+        print(value)
